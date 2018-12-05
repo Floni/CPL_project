783ff9347c4a418452d10ebbd98da2086f756f43
Florian Van Heghe <florianvanheghe@gmail.com>
2018-12-05 22:44:08 +0100

    Added more TODOs

--- src/main/scala/viw/Viw.scala
+++ src/main/scala/viw/Viw.scala
@@ -7,15 +7,22 @@ object Viw {
   def processKey(key: String, state: State) : Option[State] = key match{
     case k if k == "h" => CursorMover.moveCursorLeft(state)
     case k if k == "l" => CursorMover.moveCursorRight(state)
+    case k if k == "k" => CursorMover.moveCursorUp(state)
+    case k if k == "j" => CursorMover.moveCursorDown(state)
     case _ => None
   }
 }
 
 // TODO: define abstract command class and subclass it for each (set of) command.
 
+// TODO: reduce code duplication by merging the move functions
+// TODO: Do this by calculating the new line and new char pos based on command and return in a general manner
+
+// TODO: Better way to copy (a part of) the state? Or edit this in some way?
+
+// TODO: Vim
+
 object CursorMover {
-  // TODO: reduce code duplication by merging the move functions
-  // TODO: Do this by calculating the new line and new char pos based on command and return in a general manner
   def moveCursorLeft(state: State) : Option[State] = {
     if (state.position.character > 0) {
       Option(State(state.content, Position(state.position.line, state.position.character - 1), None, state.mode))
@@ -32,12 +39,14 @@ object CursorMover {
       Option(State(state.content, Position(state.position.line, state.position.character + 1), None, state.mode))
     } else if (state.position.line < state.contentLines.length) {
       val newLine = state.position.line + 1
-      Option(State(state.content, Position(newLine, state.contentLines(newLine).length), None, state.mode))
+      Option(State(state.content, Position(newLine, 0), None, state.mode))
     } else {
       Option(state)
     }
   }
 
+  // TODO: What if the line we move to is shorter than character pos? Just setting it to the length of that line is not
+  // TODO: right because when moving again to another line that has length at least as large as the char pos, then the char pos will again be the original
   def moveCursorUp(state: State) : Option[State] = {
     if (state.position.line > 0) {
       Option(State(state.content, Position(state.position.line - 1, state.position.character), None, state.mode))
