67afe4a1a67001781ee899ef962de95d1d97a373
Florian Van Heghe <florianvanheghe@gmail.com>
2018-12-07 19:18:25 +0100

    Added start and end of line movement commands

--- src/main/scala/viw/Viw.scala
+++ src/main/scala/viw/Viw.scala
@@ -11,7 +11,12 @@ object Viw {
     "h" -> MoveLeftCommand,
     "j" -> MoveDownCommand,
     "k" -> MoveUpCommand,
-    "l" -> MoveRightCommand
+    "l" -> MoveRightCommand,
+    "w" -> NextWordCommand,
+    "b" -> BackWordCommand,
+    "e" -> EndWordCommand,
+    "$" -> EndLineCommand,
+    "0" -> StartLineCommand
   )
 
   def processKey(key: String, state: State) : Option[State] = {
@@ -27,36 +32,79 @@ abstract class Command(state: State) {
   def eval: Option[State]
 }
 
-abstract class MoveCommand(state: State) extends Command(state) {}
+abstract class MoveCommand(state: State) extends Command(state) {
+  val line = state.position.line
+  val char = state.position.character
+  val contentLines = state.contentLines
+  val position = state.position
+}
 
 case class MoveLeftCommand(state: State) extends MoveCommand(state) {
   def eval: Option[State] = {
-    val newPos = Position(state.position.line, max(0, state.position.character - 1))
-    Option(state.copy(position = newPos))
+    val newPos = Position(state.position.line, max(0, char - 1))
+    Some(state.copy(position = newPos))
   }
 }
 
 case class MoveDownCommand(state: State) extends MoveCommand(state) {
   def eval: Option[State] = {
-    val newPos = if (state.position.line < state.contentLines.length - 1) {
-      Position(state.position.line + 1, min(state.contentLines(state.position.line + 1).length, state.position.character))
+    val newPos = if (state.position.line < contentLines.length - 1) {
+      Position(line + 1, min(contentLines(line + 1).length, char))
     } else state.position
-    Option(state.copy(position = newPos))
+    Some(state.copy(position = newPos))
   }
 }
 
 case class MoveUpCommand(state: State) extends MoveCommand(state) {
   def eval: Option[State] = {
     val newPos = if (state.position.line > 0) {
-      Position(state.position.line - 1, min(state.contentLines(state.position.line - 1).length, state.position.character))
+      Position(line - 1, min(contentLines(line - 1).length, char))
     } else state.position
-    Option(state.copy(position = newPos))
+    Some(state.copy(position = newPos))
   }
 }
 
 case class MoveRightCommand(state: State) extends MoveCommand(state) {
   def eval: Option[State] = {
-    val newPos = Position(state.position.line, min(state.contentLines(state.position.line).length - 1, state.position.character + 1))
-    Option(state.copy(position = newPos))
+    val newPos = Position(line, min(contentLines(line).length - 1, char + 1))
+    Some(state.copy(position = newPos))
+  }
+}
+
+abstract class MoveWordCommand(state: State) extends MoveCommand(state) {
+  def words(line: String): Array[String] = {
+    line.split(' ')
+  }
+}
+
+case class NextWordCommand(state: State) extends MoveWordCommand(state) {
+  def eval: Option[State] = {
+    val whitespace = contentLines(line).indexOf(' ', char)
+    val character = contentLines(line).indexOf(' ', char)
+    Some(state)
+  }
+}
+
+case class BackWordCommand(state: State) extends MoveWordCommand(state) {
+  def eval: Option[State] = {
+    Some(state)
+  }
+}
+
+case class EndWordCommand(state: State) extends MoveWordCommand(state) {
+  def eval: Option[State] = {
+    Some(state)
+  }
+}
+
+case class EndLineCommand(state: State) extends MoveCommand(state) {
+  def eval: Option[State] = {
+    Some(state.copy(position = position.copy(character = contentLines(line).length - 1)))
+  }
+}
+
+case class StartLineCommand(state: State) extends MoveCommand(state) {
+  def eval: Option[State] = {
+    Some(state.copy(position = position.copy(character = 0)))
   }
 }
